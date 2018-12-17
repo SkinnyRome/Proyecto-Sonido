0750710e4874631ab394d39f4212b3bdfae67451
SkinnyRome <angromer@ucm.es>
2018-12-17 19:31:35 +0100

    First commit

---
+++ Proyecto Sonido/Assets/Scripts/FMODExample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODExample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var lowlevelSystem = FMODUnity.RuntimeManager.LowlevelSystem;
+        uint version;
+        lowlevelSystem.getVersion(out version);
+
+
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
