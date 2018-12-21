15b4544fad5ff63116fd77c79f6ad71cb03884c4
pachi <carsan15@ucm.es>
2018-12-21 15:22:52 +0100

    juego basico

---
+++ Proyecto Sonido/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+
+
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+    private Vector3 rotationOffset;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+        rotationOffset = transform.rotation.eulerAngles - player.transform.rotation.eulerAngles;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        //transform.Rotate(rotationOffset * gameObject.transform.up);
+        //transform.LookAt(player.transform);
+        //transform.position = transform.localPosition;
+        //transform.localEulerAngles = new Vector3(0, player.transform.eulerAngles.y, 0);
+        //transform.position = player.transform.position + offset;
+
+
+
+    }
+}
---
+++ Proyecto Sonido/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    public int playerId = -1;
+
+    const float slowFactor = 2.0f;
+
+    private float jumpSpeed = 8;
+    private Rigidbody rigidbody;
+    private bool canJump;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+        canJump = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //Rotate
+        gameObject.transform.Rotate(gameObject.transform.up * (Input.GetAxis("Mouse X")));
+
+        //Move
+        gameObject.transform.Translate(((Input.GetAxis("Horizontal") / slowFactor)), 0, ((Input.GetAxis("Vertical") / slowFactor)));
+        float f = (((Input.GetAxis("Vertical") + 1) / 2) * 255);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (canJump) {
+                canJump = false;
+                rigidbody.velocity += jumpSpeed * Vector3.up;
+            }
+        }
+
+
+
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Suelo")
+        {
+            canJump = true;
+        }
+    }
+}
